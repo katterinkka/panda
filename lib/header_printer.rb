bc353be78fcd6347c2407b3545e0bc9e3f69f244
Kate Nazarova <katterinkka@gmail.com>
2014-03-19 12:26:55 +0400

    В первый раз добавили файлы

---
+++ lib/header_printer.rb
@@ -0,0 +1,32 @@
+#encoding: utf-8
+
+class HeaderPrinter
+
+	def initialize(text)
+	  @text = text
+	end 
+
+  def print_header_a
+  	print_separator('*')
+    print_text
+    print_separator('*')
+  end
+
+  def print_header_p
+  	print_separator('+')
+    print_text
+    print_separator('+')
+  end
+
+  private
+
+	def print_text
+		puts @text
+	end
+
+	def print_separator(symbol = "*", times = 67)
+		puts symbol * times
+  end
+
+end
+ 
---
+++ lib/print_header.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+require 'header_printer'
+
+HeaderPrinter.new("Всем привет").print_header_a
+puts
+HeaderPrinter.new("Привет, Катя").print_header_p
